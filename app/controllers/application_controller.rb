9c29c29305f5a6b0d362034e0395ac036fa1774c
Bui Ngoc May <ngocmay1597@gmail.com>
2018-09-24 13:51:38 +0700

    Chapter 4: Rails-flavored Ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
+  before_action :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
