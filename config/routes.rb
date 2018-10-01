67f8b3cc7726599704c302f41d8815628d8be40a
Bui Ngoc May <ngocmay1597@gmail.com>
2018-10-01 14:28:24 +0700

    Chapter 11: Account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    default_url_options :host => "localhost:3000"
     root "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users
+    resources :account_activations, only: [:edit]
   end
 end
