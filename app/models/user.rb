e5eddcae28f99d10f9c0437f943dc77e52c7c0f7
Bui Ngoc May <ngocmay1597@gmail.com>
2018-10-08 11:55:28 +0700

    Chapter 12: Password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_reader :remember_token, :activation_token
+  attr_reader :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -56,6 +56,19 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    @reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
   def downcase_email
@@ -63,7 +76,7 @@ class User < ApplicationRecord
   end
 
   def create_activation_digest
-    @activation_token  = User.new_token
-    @activation_digest = User.digest(activation_token)
+    @activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
