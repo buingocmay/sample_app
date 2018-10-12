a45b5d52b2fbf98a5d351c19599d2890a58860e5
Bui Ngoc May <ngocmay1597@gmail.com>
2018-10-12 09:54:56 +0700

    Chapter 13: User microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,17 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  mount_uploader :picture, PictureUploader
+
+  validates :content, presence: true, length: {maximum: Settings.max_length_post}
+  validates :user, presence: true
+  validate :picture_size
+
+  scope :order_desc, ->{order created_at: :desc}
+
+  private
+
+  def picture_size
+    return unless picture.size > Settings.size_image_mb.megabytes
+    errors.add(:picture, I18n.t("max_size_file"))
+  end
+end
